d9d66fe0f1db00c4e400ae6a888241aed0219191
danielmitiku <wedanielmitiku@gmail.com>
2019-12-13 19:14:16 +0300

    added fetchWeather function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import './style.css';
 
+const APPID = 'f7b60f6f82b5a121d3e5692ae0173a3d';
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather';
+
 const fetchWeather = (location) => {
     let loc = location;
-    fetch('url.url.com/api', { mode: 'cors'}).then(function(response){
-        
-    }).catch(function(err){
+    let urlVar = baseURL + '?q=' + loc + '&APPID=' + APPID; 
+    fetch(urlVar, { mode: 'cors'})
+        .then((response) => {
+            return response.json();
+        })
+        .then((response) => {
+            let data = response;
+            console.log(data);
+            console.log(weather(response));
+        })
+        .catch((err) => {
 
-    });
+        });
 };
+
+const weather = (weatherJson) => {
+    const tempKelvin = weatherJson.main.temp;
+    const tempCelsius = parseFloat((tempKelvin - 273.15).toFixed(2));
+    const tempFahrenheit = parseFloat(((tempCelsius) * 9/5 + 32).toFixed(2));
+    return { "kelvin" : tempKelvin, "celsius": tempCelsius, "fahrenheit": tempFahrenheit };
+};
+
+fetchWeather('London,uk');
