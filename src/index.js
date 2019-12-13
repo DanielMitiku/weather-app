ea3f35cba5fbe7ab87cebe995b39ae193914d03c
danielmitiku <wedanielmitiku@gmail.com>
2019-12-13 17:47:22 +0300

    added lints

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './style.css';
+
+const fetchWeather = (location) => {
+    let loc = location;
+    fetch('url.url.com/api', { mode: 'cors'}).then(function(response){
+        
+    }).catch(function(err){
+
+    });
+};
