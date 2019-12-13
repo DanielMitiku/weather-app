e1e921ed83ec5a13caec77f90347dbae5466fae2
danielmitiku <wedanielmitiku@gmail.com>
2019-12-14 00:59:06 +0300

    finished project requirement

--- src/index.js
+++ src/index.js
@@ -3,28 +3,68 @@ import './style.css';
 const APPID = 'f7b60f6f82b5a121d3e5692ae0173a3d';
 const baseURL = 'http://api.openweathermap.org/data/2.5/weather';
 
-const fetchWeather = (location) => {
+const fetchWeather = (location, unit) => {
     let loc = location;
-    let urlVar = baseURL + '?q=' + loc + '&APPID=' + APPID; 
+    let u = unit;
+    let urlVar = baseURL + '?q=' + loc + '&APPID=' + APPID;
+    loader(); 
     fetch(urlVar, { mode: 'cors'})
         .then((response) => {
-            return response.json();
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            else {
+                return response.json();
+            }
         })
         .then((response) => {
+            console.log(response);
             let data = response;
-            console.log(data);
-            console.log(weather(response));
+            const weatherObj = weather(data, u);
+            console.log(weatherObj);
+            showWeather(weatherObj, u);
         })
         .catch((err) => {
-
+            showMessage('Location could not be found. Please try again later!', 'danger');
         });
 };
 
-const weather = (weatherJson) => {
+const weather = (weatherJson, u) => {
     const tempKelvin = weatherJson.main.temp;
     const tempCelsius = parseFloat((tempKelvin - 273.15).toFixed(2));
     const tempFahrenheit = parseFloat(((tempCelsius) * 9/5 + 32).toFixed(2));
-    return { "kelvin" : tempKelvin, "celsius": tempCelsius, "fahrenheit": tempFahrenheit };
+    if (u === 'celsius') {
+        return { "celsius": tempCelsius };
+    }
+    else if (u === 'kelvin') {
+        return { "kelvin" : tempKelvin };
+    }
+    else if (u === 'fahrenheit') {
+        return { "fahrenheit": tempFahrenheit };
+    }
+};
+
+const loader = () => {
+    const resultDiv = document.querySelector('.result');
+    const resultP = document.createElement('h2');
+    resultP.classList.add('text-center');
+    resultDiv.innerText = "...loading...";
+    resultDiv.appendChild(resultP);
+};
+
+const showWeather = (tempObj, u) => {
+    const resultDiv = document.querySelector('.result');
+    resultDiv.innerHTML = '';
+    const resultP = document.createElement('h2');
+    resultP.classList.add('text-center');
+    if (u === 'celsius') {
+        resultP.innerHTML = `The Temperature of the place you entered is ${tempObj.celsius}&#8451;`;}
+    else if (u === 'kelvin') {
+        resultP.innerHTML = `The Temperature of the place you entered is ${tempObj.kelvin}K`;}
+    else if (u === 'fahrenheit') {
+        resultP.innerHTML = `The Temperature of the place you entered is ${tempObj.fahrenheit}&#8457`;}
+    
+    resultDiv.appendChild(resultP);
 };
 
 const showMessage = (message, className) => {
@@ -35,6 +75,7 @@ const showMessage = (message, className) => {
     const form = document.querySelector('.weather-form');
     messageContainer.insertBefore(div, form);
     setTimeout(() => document.querySelector('.alert').remove(), 3000);
+    document.querySelector('.result').innerHTML = '';
 };
 
 const acceptLocation = () => {
@@ -42,8 +83,10 @@ const acceptLocation = () => {
     form.addEventListener('submit', (event) => {
         event.preventDefault();
         const city = form.querySelector('#city').value;
-        if (city !== '') {
-            
+        const unit = document.querySelector('#unit').value;
+        if (city !== '' && unit !== '') {
+            document.querySelector('.result').innerHTML = '';
+            fetchWeather(city, unit);
             document.querySelector('#city').value = '';
         }
         else {
@@ -54,4 +97,8 @@ const acceptLocation = () => {
 
 acceptLocation();
 
-fetchWeather('London,uk');
+
+// fetchWeather('London,uk');
+//console.log(fetchWeather('London,uk'));
+//const getWeather = async () => await fetchWeather('London,uk');
+//console.log(getWeather);
